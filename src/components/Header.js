699ac31415964dc7ad942530e2f27ed7037a7a07
coderBhav <bhch0204@gmail.com>
2025-08-29 14:46:29 +0530

    updating files for github pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 
-const Header=()=> {
+const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
+
   const toggleTheme = () => {
     setDarkMode(!darkMode);
     document.body.classList.toggle("dark-mode");
   };
+
   return (
     <header className="app-header">
       <div className="heading">
-        <h1 className="logo"><i className="material-icons">location_on</i>Location Tracker</h1>
+        <h1 className="logo">
+          <i className="material-icons location-icon">location_on</i>
+          Location Tracker
+        </h1>
         <button className="toggle-btn" onClick={toggleTheme}>
           <i className={`fas ${darkMode ? "fa-sun" : "fa-moon"}`}></i>
         </button>
       </div>
-      <p>Track your live location and share it easily!</p>
+      <p className="tagline">Track your live location and share it easily!</p>
     </header>
   );
 };
